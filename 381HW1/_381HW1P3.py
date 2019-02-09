45bef96f50d9edac761aef7a2f4a44e835300b89
Tajbir Sandhu <sandhu_tajbir@outlook.com>
2019-02-09 15:13:37 -0800

    Fixed a typo in the for loop for problem 3

--- 381HW1/_381HW1P3.py
+++ 381HW1/_381HW1P3.py
@@ -8,11 +8,11 @@ def main():
     counter = 0
     for i in range(0, N):
         hits = 0
-        for j in range(0, 101):
+        for j in range(0, 100):
             if(random.randint(0,2) == 1):
                 hits = hits + 1
         if(hits == 50):
             counter = counter + 1
-    print(counter)
+    print("Probability: ", counter/N)
 
 main()
