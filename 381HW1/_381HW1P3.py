3c68572b558c8666e9e33f0b84fa8eb51d08bb87
Tajbir Sandhu <sandhu_tajbir@outlook.com>
2019-02-11 17:24:04 -0800

    Added comments that should've been there in the first place, removed one line function

--- 381HW1/_381HW1P3.py
+++ 381HW1/_381HW1P3.py
@@ -4,15 +4,16 @@ import matplotlib.pyplot as plt
 import random
 
 def main():
-    N = 100000
-    counter = 0
+    N = 100000                                  #repitions
+    counter = 0                                 #counter for the number of times 50 heads/tails have been flipped
+    #outside loop which repeats experiment 100000 times
     for i in range(0, N):
-        hits = 0
-        for j in range(0, 100):
-            if(random.randint(0,2) == 1):
-                hits = hits + 1
+        hits = 0                                #records the number of times 1 (heads) is flipped
+        for j in range(0, 100):                 #inside loop to flip a fair coin 100 times
+            if(random.randint(0,2) == 1):       
+                hits = hits + 1                 #increment the hits counter if 1(heads) is flipped
         if(hits == 50):
-            counter = counter + 1
-    print("Probability: ", counter/N)
+            counter = counter + 1               #increment the counter if after flipping a coin 100 times, 50 heads were flipped
+    print("Probability: ", counter/N)           #prints out the probability
 
 main()
--- 381HW1/_381HW1P4.py
+++ 381HW1/_381HW1P4.py
@@ -4,42 +4,48 @@ import matplotlib.pyplot as plt
 import random
 import string
 
-def randString(n):
-    return ''.join(random.choice(string.ascii_lowercase) for x in range(n))
 
 def main():
-    m = 80000   #passwords
-    k = 7
-    N = 1000    #repitions
-    count = 0
-    passwords = list()
+    #initializations
+    m = 80000                                                                                       #passwords
+    k = 7                                                                                           #multiplier for the number of passwords
+    N = 1000                                                                                        #repitions
+    count = 0                                                                                       #counter for the number of matches
+    passwords = list()                                                                              #list to store hacker passwords
     
+    #loop to create 80000 passwords
     for i in range(0, m):
-        passwords.append(randString(4))
+        passwords.append(''.join(random.choice(string.ascii_lowercase) for x in range(4)))          #creates a random string of length 4
+    #loop to determine the number of matches in a list of 80000 passwords
     for i in range(0, N):
-        yourPass = randString(4)
+        yourPass = ''.join(random.choice(string.ascii_lowercase) for x in range(4))
         if(yourPass in passwords):
-            count = count + 1
-    print("Probability: ", count/N)
-    passwords.clear()
-    count = 0
+            count = count + 1                                                                       #increment the counter if some password in the list matches the key
+    print("Probability: ", count/N)                                                                 #displays the probability
+    passwords.clear()                                                                               #empties the list of passwords
+    count = 0                                                                                       #resets the count for the next loop
+
+    #loop to create 80000*7 passwords
     for i in range(0, m*k):
-        passwords.append(randString(4))
+        passwords.append(''.join(random.choice(string.ascii_lowercase) for x in range(4)))          #creates a random string of length 4
+    #loop to determine the number of matches in a list of 80000*7 passwords
     for i in range(0, N):
-        yourPass = randString(4)
+        yourPass = ''.join(random.choice(string.ascii_lowercase) for x in range(4))
         if(yourPass in passwords):
-            count = count + 1
-    print("Probability: ", count/N)
-    passwords.clear()
-    
-    count = 0
+            count = count + 1                                                                       #increment the counter if some password in the list matches the key
+    print("Probability: ", count/N)                                                                 #displays the probability
+    passwords.clear()                                                                               #empties the list of passwords
+    count = 0                                                                                       #resets the count for the next loop
+
+    #loop to create 80000*4 passwords
     for i in range(0, m*4):
-        passwords.append(randString(4))
+        passwords.append(''.join(random.choice(string.ascii_lowercase) for x in range(4)))          #creates a random string of length 4
+    #loop to determine the number of matches in a list of 80000*4 passwords
     for i in range(0, N):
-        yourPass = randString(4)
+        yourPass = ''.join(random.choice(string.ascii_lowercase) for x in range(4))
         if(yourPass in passwords):
-            count = count + 1
-    print("Probability: ", count/N)
+            count = count + 1                                                                       #increment the counter if some password in the list matches the key
+    print("Probability: ", count/N)                                                                 #displays the probability
 
 
 main()
